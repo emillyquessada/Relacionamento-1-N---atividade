1214925b6b1f9723d75d6a98e223190bcd2b3d6a
Emilly <emillyquessada26@gmail.com>
2026-04-06 16:16:05 -0300

    Estrutura Inicial: criação de tabelas

---
+++ main.py
@@ -0,0 +1,39 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+
+Base = declarative_base()
+
+class Artista(Base):
+    __tablename__ = "artistas"
+
+    id_artista = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(100), nullable=False)
+    nascimento = Column(Integer)
+
+    albuns = relationship("Album", back_populates="artista")
+
+    def __repr__(self):
+        return f"Artista: ID = {self.id_artista} | Nome = {self.nome} | Ano Nascimento = {self.nascimento}"
+    
+class Album(Base):
+    __tablename__= "albuns"
+
+    id_album =Column(Integer, primary_key=True, autoincrement=True)
+    nome_album = Column(String(100), nullable=False)
+    musicas = Column(Integer, nullable=False)
+    estilo = Column(String(100))
+    lancamento = Column(Integer, nullable=False)
+
+    artista_id = Column(Integer, ForeignKey("artistas.id_artista"))
+
+    artista = relationship("Artista", back_populates="albuns")
+
+
+    def __repr__(self):
+        return f"Álbum: ID = {self.id_album} | Nome = {self.nome_album} | Quantidade de Músicas = {self.musicas} | Estilo = {self.estilo} | Data de Lançamento = {self.lancamento}"
+    
+engine = create_engine("sqlite:///spotify.db")
+
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+
